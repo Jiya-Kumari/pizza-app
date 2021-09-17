f0b4091140ca0bca82862c0ef22f386273dfa66a
Jiya-Kumari <89457678+Jiya-Kumari@users.noreply.github.com>
2021-09-17 10:36:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+menu_list_array=["Veg Margherita Pizza",
+                 "Chicken Tandoori Pizza",
+                 "Deluxe Veggie Pizza",
+                 "Paneer Tikka Pizza",
+                 "Double Cheese Pizza",
+                 "Cheese and Corn Pizza",
+                 "Cheese Loaded Pizza",
+                 "Veg Extravaganza Pizza",
+document.getElementById=(" ").innerHTML= htmldata;
+] 
